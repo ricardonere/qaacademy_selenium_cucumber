1be28c4bb694fb28b1b575ef37a5315b04c1a4c7
Ricardo Nere <ricardonere@gmail.com>
2022-05-24 15:06:46 -0300

    Teste Page Bugbank

---
+++ src/test/java/pages/HomePage.java
@@ -0,0 +1,70 @@
+package pages;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HomePage {
+    WebDriver driver;
+    String botaoRegistrar = "//button[contains(text(),'Registrar')]";
+    String email = "//body/div[@id='__next']/div[1]/div[2]/div[1]/div[2]/form[1]/div[2]/input[1]";
+    String nome = "//body/div[@id='__next']/div[1]/div[2]/div[1]/div[2]/form[1]/div[3]/input[1]";
+    String senha = "//body/div[@id='__next']/div[1]/div[2]/div[1]/div[2]/form[1]/div[4]/div[1]/input[1]";
+    String confirmarSenha = "//body/div[@id='__next']/div[1]/div[2]/div[1]/div[2]/form[1]/div[5]/div[1]/input[1]";
+    String criarComSaldo = "//label[@id='toggleAddBalance']";
+    String cadastrar = "//button[contains(text(),'Cadastrar')]";
+
+    String fecharModal = "//*[@id='btnCloseModal']";
+
+    String emailLogin = "//body/div[@id='__next']/div[1]/div[2]/div[1]/div[1]/form[1]/div[1]/input[1]";
+
+    String senhaLogin = "//body/div[@id='__next']/div[1]/div[2]/div[1]/div[1]/form[1]/div[2]/div[1]/input[1]";
+
+    String botaoAcessar = "//button[contains(text(),'Acessar')]";
+    public HomePage(WebDriver driverDoTeste) {
+        this.driver = driverDoTeste;
+    }
+
+
+    public void clicarRegistrar() {
+        driver.findElement(By.xpath(botaoRegistrar)).click();
+    }
+
+    public void preencherEmail() {
+        driver.findElement(By.xpath(email)).sendKeys("teste@teste.com");
+    }
+
+    public void preencherNome() {
+        driver.findElement(By.xpath(nome)).sendKeys("QA Ricardo");
+    }
+
+    public void digitarSenha() {
+        driver.findElement(By.xpath(senha)).sendKeys("415263");
+    }
+
+    public void confirmarSenha() {
+        driver.findElement(By.xpath(confirmarSenha)).sendKeys("415263");
+    }
+
+    public void clicarCriarContaComSaldo() {
+        driver.findElement(By.xpath(criarComSaldo)).click();
+    }
+
+    public void clicarCadastrar() {
+        driver.findElement(By.xpath(cadastrar)).click();
+    }
+    public void fecharBotaoModal() throws InterruptedException {
+        driver.findElement(By.xpath(fecharModal)).click();
+        Thread.sleep( 1000);
+    }
+    public void preencherEmailLogin(){
+        driver.findElement(By.xpath(emailLogin)).sendKeys("teste@teste.com");
+    }
+    public void preencherSenhaLogin(){
+        driver.findElement(By.xpath(senhaLogin)).sendKeys("415263");
+    }
+    public void clicarAcessar(){
+        driver.findElement(By.xpath(botaoAcessar)).click();
+    }
+}
---
+++ src/test/java/scenarios/TesteCadastro.java
@@ -0,0 +1,41 @@
+package scenarios;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import pages.HomePage;
+
+import java.util.concurrent.TimeUnit;
+
+public class TesteCadastro {
+    WebDriver driver;
+    HomePage homePage;
+
+    @Before
+    public void setup() throws InterruptedException {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        homePage = new HomePage(driver);
+        driver.get("http://localhost:3000/");
+    }
+    @Test
+    public void testeCadastro() throws InterruptedException {
+        homePage.clicarRegistrar();
+        homePage.preencherEmail();
+        homePage.preencherNome();
+        homePage.digitarSenha();
+        homePage.confirmarSenha();
+        homePage.clicarCriarContaComSaldo();
+        homePage.clicarCadastrar();
+        homePage.fecharBotaoModal();
+        homePage.preencherEmailLogin();
+        homePage.preencherSenhaLogin();
+        homePage.clicarAcessar();
+    }
+    @After
+    public void encerra(){
+        driver.quit();
+    }
+}
